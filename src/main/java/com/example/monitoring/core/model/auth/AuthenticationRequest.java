351096548abee438c42b6267ca06077d5b5dc65a
mikoMaz <mikmaz1@st.amu.edu.pl>
2024-04-21 13:36:18 +0200

    add endpoints

---
+++ src/main/java/com/example/monitoring/core/model/auth/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.example.monitoring.core.model.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    String deviceId;
+    String deviceType;
+}
---
+++ src/main/java/com/example/monitoring/core/model/auth/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.example.monitoring.core.model.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
