256fdb21b55c667806b98023e04f5fbe4d955478
VVojtekMecki <wojtek.medenecki@gmail.com>
2024-05-11 20:01:04 +0200

    logo component

---
+++ src/main/webclient/src/components/layout/navbar/components/logo/logo.tsx
@@ -0,0 +1,4 @@
+import { Image } from "@chakra-ui/react";
+export const Logo = () => {
+	return <Image src="/public/img/logo192.png" alt="Logo" />
+}
--- src/main/webclient/src/components/layout/navbar/navbar.tsx
+++ src/main/webclient/src/components/layout/navbar/navbar.tsx
@@ -1,19 +1,20 @@
-import { Grid, GridItem, Image } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 import { NavigationButtons } from "./components/navigation-buttons/navigation-buttons";
 import { UserDropdown } from "./components/user-dropdown/user-dropdown";
+import { Logo } from "./components/logo/logo";
 
 export const Navbar = () => {
   return (
     <Grid templateColumns="1fr 1fr 5fr 2fr 2fr" gap={2}>
       <GridItem>
-        <Image src="/public/img/logo192.png" alt="Logo" />
+        <Logo />
       </GridItem>
       <GridItem>
         <div>Name</div>
       </GridItem>
       <GridItem>
-		<div className="navbar-empty-space"/>
-	  </GridItem>
+        <div className="navbar-empty-space" />
+      </GridItem>
       <GridItem>
         <NavigationButtons />
       </GridItem>
