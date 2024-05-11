5e6ade5c18a3837283192258ef7186c78e2bb30b
VVojtekMecki <wojtek.medenecki@gmail.com>
2024-05-11 20:26:04 +0200

    fixed app params definition

--- src/main/webclient/src/App.tsx
+++ src/main/webclient/src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "./App.css";
 import "@mantine/core/styles.css";
-import { AppMain } from "./components/app-main/app-main";
+import { AppMain, IAppMainProps } from "./components/app-main/app-main";
 
 export default function App() {
-
-  return <AppMain />;
+  const appMainProps: IAppMainProps = {
+    ui: {
+      colors: {
+        white: "#FFFFFF",
+        black: "#000000",
+        primary: "#22543D",
+        secondary: "#9AE6B4",
+        accent: "#CBD5E0",
+        background: "#F4F3F0",
+      },
+    },
+  };
+  return <AppMain {...appMainProps} />;
 }
--- src/main/webclient/src/components/app-main/app-main.tsx
+++ src/main/webclient/src/components/app-main/app-main.tsx
@@ -5,23 +5,9 @@ import { ProjectColorsDictionary } from "../../types/projectTypes";
 export interface IAppMainProps {
   ui: IUIProps
 }
-
 export interface IUIProps {
   colors: ProjectColorsDictionary;
 }
-
-const appMainProps: IAppMainProps = {
-  ui: {
-    colors: {
-      white: "#FFFFFF",
-      black: "#000000",
-      primary: "#22543D",
-      secondary: "#9AE6B4",
-      accent: "#CBD5E0",
-      background: "#F4F3F0",
-    },
-  },
-};
 
 export const AppMain = ({...appMainProps}: IAppMainProps) => {
   return (
