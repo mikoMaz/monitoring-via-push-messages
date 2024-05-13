601f08f6d75a1d0ef80be4ce76939c385a04184e
mikoMaz <mikmaz1@st.amu.edu.pl>
2024-05-13 15:29:10 +0200

    delete crashing import

--- src/main/java/com/example/monitoring/core/api/auth/AuthenticationController.java
+++ src/main/java/com/example/monitoring/core/api/auth/AuthenticationController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.fasterxml.jackson.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
