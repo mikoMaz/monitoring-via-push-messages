46a617cd8ad18066ac61730fd817f30e96e72e39
VVojtekMecki <wojtek.medenecki@gmail.com>
2024-05-19 16:09:14 +0200

    filter section input

--- src/main/webclient/src/components/layout/button-with-icon.tsx
+++ src/main/webclient/src/components/layout/button-with-icon.tsx
@@ -1,22 +1,24 @@
 import { Button, ButtonProps, theme } from "@chakra-ui/react";
 import { IUIProps } from "../app-main/app-main";
+import { appProps } from "../../config/config";
 
 interface IButtonWithIconProps {
   icon: React.ReactElement;
   text: string;
-  ui: IUIProps;
   props?: ButtonProps;
 }
 
-export const ButtonWithIcon = ({
-  text,
-  icon,
-  ui,
-  props,
-}: IButtonWithIconProps) => {
+export const ButtonWithIcon = ({ text, icon, props }: IButtonWithIconProps) => {
+  const ui = appProps.appMainProps.ui;
   return (
     //ustawic theme
-    <Button leftIcon={icon} {...props} colorScheme="secondary" color={ui.colors.black} variant="solid">
+    <Button
+      leftIcon={icon}
+      {...props}
+      colorScheme="secondary"
+      color={ui.colors.black}
+      variant="solid"
+    >
       {text}
     </Button>
   );
--- src/main/webclient/src/components/monitoring-page/components/filter-section-buttons.tsx
+++ src/main/webclient/src/components/monitoring-page/components/filter-section-buttons.tsx
@@ -1,30 +1,52 @@
-import { Button, Grid, GridItem } from "@chakra-ui/react";
+import {
+  Button,
+  Grid,
+  GridItem,
+  IconProps,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
 import { ButtonWithIcon } from "../../layout/button-with-icon";
 import { IUIProps } from "../../app-main/app-main";
-import FilterListIcon from '@mui/icons-material/FilterList';
-
+import FilterListIcon from "@mui/icons-material/FilterList";
+import FilterAltOutlinedIcon from "@mui/icons-material/FilterAltOutlined";
+import NumbersIcon from "@mui/icons-material/Numbers";
 
 interface IFilterSectionButtonsProps {
-  ui: IUIProps
+  ui: IUIProps;
 }
 
-export const FilterSectionButtons = ({ui}: IFilterSectionButtonsProps) => {
+export const FilterSectionButtons = ({ ui }: IFilterSectionButtonsProps) => {
   return (
-    <Grid templateColumns="2fr 1fr 2fr 1fr 2fr">
+    <Grid templateColumns="2fr 1fr 2fr 1fr 3fr">
       <GridItem>
-        <ButtonWithIcon text="Sort" icon={<FilterListIcon/>} ui={ui}/>
+        <ButtonWithIcon
+          text="Sort"
+          icon={<FilterListIcon />}
+          props={{ fontSize: "14px", size: "md" }}
+        />
       </GridItem>
       <GridItem>
-        <div className="empty-space"/>
+        <div className="empty-space" />
       </GridItem>
       <GridItem>
-        test
+        <ButtonWithIcon
+          text="Filter"
+          icon={<FilterAltOutlinedIcon />}
+          props={{ fontSize: "14px", size: "md" }}
+        />
       </GridItem>
       <GridItem>
-        <div className="empty-space"/>
+        <div className="empty-space" />
       </GridItem>
       <GridItem>
-        test
+        <InputGroup>
+          <InputLeftElement>
+            <NumbersIcon />
+          </InputLeftElement>
+          <Input placeholder="Device ID" focusBorderColor="yellow" />
+        </InputGroup>
       </GridItem>
     </Grid>
   );
