69f1cbe07a58a2ba06bf284795b3620a40e95a11
VVojtekMecki <wojtek.medenecki@gmail.com>
2024-05-27 13:29:51 +0200

    use effect shoud run only once

--- src/main/webclient/src/App.tsx
+++ src/main/webclient/src/App.tsx
@@ -3,16 +3,18 @@ import "./App.css";
 import "@mantine/core/styles.css";
 import { AppMain } from "./components/app-main/app-main";
 import { useNavigate } from "react-router-dom";
-import { appProps } from './config/config';
+import { appProps } from "./config/config";
 
 export default function App() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    document.body.style.backgroundColor = appProps.appMainProps.ui.colors.background
+    document.body.style.backgroundColor =
+      appProps.appMainProps.ui.colors.background;
     if (window.location.pathname === "/") {
       navigate("/monitoring", { replace: true });
     }
-  });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return <AppMain {...appProps.appMainProps} />;
 }
