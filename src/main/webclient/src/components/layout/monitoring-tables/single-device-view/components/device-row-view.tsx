79a3c6565ccaea468ff93196ca48c818e697b9d2
VVojtekMecki <wojtek.medenecki@gmail.com>
2024-05-28 15:55:17 +0200

    single device view edits

--- src/main/webclient/src/components/layout/monitoring-tables/single-device-view/components/device-row-view.tsx
+++ src/main/webclient/src/components/layout/monitoring-tables/single-device-view/components/device-row-view.tsx
@@ -1,11 +1,16 @@
-import { Td, Tr } from "@chakra-ui/react";
+import { HStack, Td, Tr } from "@chakra-ui/react";
 import { IMonitoringDevice } from "../../../../../types/deviceModel";
 import { StatusDotIndicator } from "../../../status-dot-indicator";
 
 export const DeviceRowView = (device: IMonitoringDevice) => {
   return (
     <Tr key={device.id}>
-      <Td>{device.id}</Td>
+      <Td>
+        <HStack>
+          <StatusDotIndicator status={device.status}/>
+          <>{device.id}</>
+        </HStack>
+      </Td>
       <Td>{device.lastPinged.toString()}</Td>
       <Td>
         <StatusDotIndicator status={device.status} />
