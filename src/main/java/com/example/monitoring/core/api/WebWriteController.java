464124c5518a9237631ce8d3f586bde010589c00
mikoMaz <mikmaz1@st.amu.edu.pl>
2024-05-29 23:14:54 +0200

    fix access to service

--- src/main/java/com/example/monitoring/core/api/WebWriteController.java
+++ src/main/java/com/example/monitoring/core/api/WebWriteController.java
@@ -7,36 +7,25 @@ import java.util.List;
 import org.aspectj.weaver.tools.Trace;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.monitoring.core.sensor.SensorDataSimplifiedService;
 import com.example.monitoring.core.bridge.BridgeData;
-import com.example.monitoring.core.bridge.BridgeRequest;
 import com.example.monitoring.core.bridge.BridgeService;
-import com.example.monitoring.core.bridge.BridgeServiceImpl;
-import com.example.monitoring.core.gateway.GatewayData;
-import com.example.monitoring.core.gateway.GatewayRepository;
-import com.example.monitoring.core.gateway.GatewayRequest;
-import com.example.monitoring.core.gateway.GatewayService;
-import com.example.monitoring.core.gateway.GatewayServiceImpl;
+
+
 @RestController
 @RequestMapping("/api/v1/")
 @RequiredArgsConstructor
 public class WebWriteController {
+    private final BridgeService bridgeService;
+
     @GetMapping("/kluczdostepu")
     public ResponseEntity<String> jsonTree(@RequestParam Integer id) {
-        GatewayServiceImpl gatewayService= new GatewayServiceImpl(null);
-        BridgeServiceImpl bridgeService=new BridgeServiceImpl(null);
         List<BridgeData> bdList;
-        bdList=bridgeService.allBridges(153);
-        
+        bdList = bridgeService.allBridges(id);
         
         return ResponseEntity.ok().body(bdList.getFirst().toString());
-
-
     }
     
 }
