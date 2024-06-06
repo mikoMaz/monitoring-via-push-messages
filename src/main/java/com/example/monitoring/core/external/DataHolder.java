f5e4ec9b0c9249b541e4099b327de90d7c9af4ad
mikoMaz <mikmaz1@st.amu.edu.pl>
2024-06-06 02:19:04 +0200

    add DataHolder and DataHolderService

---
+++ src/main/java/com/example/monitoring/core/external/DataHolder.java
@@ -0,0 +1,42 @@
+package com.example.monitoring.core.external;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Setter
+@Getter
+@Component
+public class DataHolder {
+    private Map<String, List<String>> deviceData = new HashMap<>();
+    private Map<String, List<String>> companyData = new HashMap<>();
+
+    /* device */
+    public void newPlainDevice(String key) {
+        deviceData.putIfAbsent(key, List.of("", ""));
+    }
+
+    public void addParentIdToDeviceData(String key, String parentId) {
+        deviceData.put(key, List.of(parentId, deviceData.get(key).get(1)));
+    }
+
+    public void addCompanyIdToDeviceData(String key, String companyId) {
+        deviceData.put(key, List.of(deviceData.get(key).getFirst(), companyId));
+    }
+
+    /* company */
+    public void newPlainCompany(String key) {
+        companyData.putIfAbsent(key, new ArrayList<>());
+    }
+
+    public void addDeviceIdToCompanyData(String key, String deviceId) {
+        if (!companyData.get(key).contains(deviceId)) {
+            companyData.get(key).add(deviceId);
+        }
+    }
+}
---
+++ src/main/java/com/example/monitoring/core/external/DataHolderService.java
@@ -0,0 +1,45 @@
+package com.example.monitoring.core.external;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class DataHolderService {
+
+    @Autowired
+    DataHolder externalData;
+
+    /* device */
+    public Map<String, List<String>> getDevice() {
+        return externalData.getDeviceData();
+    }
+
+    public void addDeviceIfNotExist(String key) {
+        externalData.newPlainDevice(key);
+    }
+
+    public void addParentIdToDeviceData(String key, String parentId) {
+        externalData.addParentIdToDeviceData(key, parentId);
+    }
+
+    public void addCompanyIdToDeviceData(String key, String companyId) {
+        externalData.addCompanyIdToDeviceData(key, companyId);
+    }
+
+
+    /* company */
+    public Map<String, List<String>> getCompany() {
+        return externalData.getCompanyData();
+    }
+
+    public void addCompanyIfNotExist(String key) {
+        externalData.newPlainCompany(key);
+    }
+
+    public void addDeviceIdToCompanyData(String key, String deviceId) {
+        externalData.addDeviceIdToCompanyData(key, deviceId);
+    }
+}
