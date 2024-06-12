36e5c66961e51ff62e09f07549d959a2430c55ee
sasankasa <96640467+sasankasa@users.noreply.github.com>
2024-06-12 22:45:21 +0200

    Mon 48/wyswietlanie wykresow (#8)
    * basic charts
    
    * removed unused imports
    
    * added pie charts with tooltip

--- src/main/webclient/src/components/dashboard-page/dashboard-page.tsx
+++ src/main/webclient/src/components/dashboard-page/dashboard-page.tsx
@@ -1,3 +1,39 @@
+import { Box, Center, HStack } from "@chakra-ui/react";
+import { appProps } from "../../config/config";
+import { useEffect, useState } from "react";
+import { DeviceModel } from "../../types/deviceModel";
+import { APIClient } from "../../api/api-client";
+import { StatusPieChart } from "./components/status-pie-chart";
+
 export const DashboardPage = () => {
-  return <>Dashboard</>;
+  const ui = appProps.appMainProps.ui;
+
+  const [deviceModel, setDeviceModel] = useState<DeviceModel>(
+    new DeviceModel()
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data: DeviceModel = APIClient.getRecentUpdates();
+      setDeviceModel(data);
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <Box
+      paddingLeft="47px"
+      paddingRight="47px"
+      bg={ui.colors.background}
+      boxShadow="inner"
+    >
+      <Center>
+        <HStack>
+          <StatusPieChart devices={deviceModel.getSensorsArray()} heading="Sensors" />
+          <StatusPieChart devices={deviceModel.getGatewaysArray()} heading="Gateways"/>
+          <StatusPieChart devices={deviceModel.getBridgesArray()} heading="Bridges"/>
+        </HStack>
+      </Center>
+    </Box>
+  );
 };
