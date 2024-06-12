ddb095fc0c1541bf8b4a64e86cf0fbef754f3b51
mikoMaz <mikmaz1@st.amu.edu.pl>
2024-06-12 20:43:08 +0200

    add dataFetch method

--- src/main/java/com/example/monitoring/core/external/ExternalDataService.java
+++ src/main/java/com/example/monitoring/core/external/ExternalDataService.java
@@ -4,6 +4,9 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +30,18 @@ public class ExternalDataService {
     public void addData(String tableName, String primaryKeyName, String secondColumn, String primaryValue, String secondValue) {
         String sql = String.format("INSERT INTO %s (%s, %s) VALUES (?, ?)", tableName, primaryKeyName, secondColumn);
         jdbcTemplate.update(sql, primaryValue, secondValue);
+    }
+
+    Map<String, String> dataFetch(String tableName, String primaryKeyName, String secondColumn) {
+        String sql = String.format("SELECT * FROM %s", tableName);
+        List<Map<String, Object>> listOfMapRows = jdbcTemplate.queryForList(sql);
+        Map<String, String> result = new HashMap<>();
+
+        for (Map<String, Object> row : listOfMapRows) {
+            String key = (String) row.get(primaryKeyName);
+            String value = (String) row.get(secondColumn);
+            result.put(key, value);
+        }
+        return result;
     }
 }
