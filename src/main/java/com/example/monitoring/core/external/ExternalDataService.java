df0b3465e7ffb151e284cc2b23bd877d35b7290b
mikoMaz <mikmaz1@st.amu.edu.pl>
2024-06-12 03:16:46 +0200

    rename class in external/

--- src/main/java/com/example/monitoring/core/external/ExternalDataService.java
+++ src/main/java/com/example/monitoring/core/external/ExternalDataService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class TableCreatorService {
+public class ExternalDataService {
 
     private final JdbcTemplate jdbcTemplate;
 
