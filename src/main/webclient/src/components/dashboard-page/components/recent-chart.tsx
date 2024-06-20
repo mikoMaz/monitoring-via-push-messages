d3d61c329310f85e66adf91728e8d29958c362df
sasankasa <michalikoliwiamagdalena@gmail.com>
2024-06-20 17:10:32 +0200

    dopisanie funkcji pod liczbę elementów

--- src/main/webclient/src/components/dashboard-page/components/recent-chart.tsx
+++ src/main/webclient/src/components/dashboard-page/components/recent-chart.tsx
@@ -1,4 +1,13 @@
-import { Bar, BarChart, Cell, Pie, PieChart, Tooltip } from "recharts";
+import {
+  Bar,
+  BarChart,
+  Cell,
+  Pie,
+  PieChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import { IMonitoringDevice } from "../../../types/deviceModel";
 import { getDevicesStatusPieChartData } from "../util/dashboard-page-util";
 import { Box, Heading, VStack } from "@chakra-ui/react";
@@ -8,26 +17,48 @@ interface IRecentChart {
   devices: number[];
 }
 
+
 export const RecentChart = ({ devices }: IRecentChart) => {
-  const colors: { [id: string]: string } = {
-    disabled: UIProps.colors.charts.disabled,
-    enabled: UIProps.colors.charts.active,
-  };
+
+function generateProgressList(numbers: any[]) {
+    const length = numbers.length;
+    if (length === 0) {
+        return [];
+    }
+    const step = 100 / (length - 1);
+    const progressList = numbers.map((num, index) => index * step);
+    return progressList;
+}
+
+const percentList = generateProgressList(devices);
 
   const getChartData = () => {
-    return devices.map((device) => {
-      return { name: device };
+    return devices.map((device, index) => {
+      return { name: percentList[index], number: device };
     });
   };
 
   return (
     <Box padding="50px">
-      <VStack>
-        <Heading>Title</Heading>
-        <BarChart width={150} height={40} data={getChartData()}>
-          <Bar dataKey="name" fill="#8884d8" />
-        </BarChart>
-      </VStack>
+      <BarChart
+        width={1200}
+        height={400}
+        data={getChartData()}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <YAxis dataKey="number" />
+        <XAxis dataKey="name" />
+        <Bar
+          dataKey="number"
+          fill={UIProps.colors.secondary}
+          label={{ position: "top" }}
+        />
+      </BarChart>
     </Box>
   );
 };
