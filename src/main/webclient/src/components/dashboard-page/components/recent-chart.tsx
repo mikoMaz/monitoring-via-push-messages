2310d7a1857041cdaf9e3799295e517e3cd8774e
sasankasa <michalikoliwiamagdalena@gmail.com>
2024-08-21 20:02:07 +0200

    dodanie tooltipa do wykresu

--- src/main/webclient/src/components/dashboard-page/components/recent-chart.tsx
+++ src/main/webclient/src/components/dashboard-page/components/recent-chart.tsx
@@ -1,4 +1,4 @@
-import { Bar, BarChart, Brush, XAxis, YAxis } from "recharts";
+import { Bar, BarChart, Brush, Tooltip, TooltipProps, XAxis, YAxis } from "recharts";
 import { Box } from "@chakra-ui/react";
 import { UIProps } from "../../../config/config";
 import { getDensityRecentChart } from "../util/density-devices-recent";
@@ -7,38 +7,77 @@ interface IRecentChart {
   devices: number[];
 }
 
+interface CustomTooltipPayload {
+  min: number;
+  max: number;
+  number: number;
+}
+
+const getDescOfPage = (payload: string | undefined, min: number, max: number) => {
+  if (payload === "0") {
+    return `There is no working device in range ${min}% - ${max}%`;
+  }
+  if (payload === "1") {
+    return `There is ${payload} working device in range ${min}% - ${max}%`;
+  }
+  return `There are ${payload} working devices in range ${min}% - ${max}%`;
+};
+
+
+const CustomTooltip = ({
+  active,
+  payload,
+  label,
+}: TooltipProps<number, string>) => {
+  if (active && payload && payload.length) {
+    const { min, max } = payload[0].payload as CustomTooltipPayload; // Dodanie typu do payload
+    return (
+      <div className="custom-tooltip">
+        <p className="label">{`${label} : ${payload[0].value}`}</p>
+        <p className="desc">
+          {getDescOfPage(payload[0].value?.toString(), min, max)}
+        </p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
 export const RecentChart = ({ devices }: IRecentChart) => {
-  const getChartData = () => {
-    return getDensityRecentChart(devices, 0.5).elements.map((device) => {
+  const chartData = getDensityRecentChart(devices, 0.5).elements.map(
+    (device) => {
       return {
-        name: device.maxPercentActivityRangePoint,
+        min: device.minPercentActivityRangePoint,
+        max: device.maxPercentActivityRangePoint,
+        name: `${device.minPercentActivityRangePoint} - ${device.maxPercentActivityRangePoint}`,
         number: device.numElements.length,
       };
-    });
-  };
+    }
+  );
 
   return (
     <Box padding="100px">
       <BarChart
         width={1200}
         height={500}
-        data={getChartData()}
+        data={chartData} // U¿ycie zmiennej
         margin={{
           top: 50,
-          right: 10,
-          left: 10,
+          right: 50,
+          left: 50,
           bottom: 50,
         }}
       >
         <YAxis dataKey="number" />
         <XAxis dataKey="name" />
-        <Brush dataKey="name" height={30} stroke={UIProps.colors.primary} />
+        <Brush dataKey="max" height={30} stroke={UIProps.colors.primary} />
         <Bar
           dataKey="number"
           fill={UIProps.colors.secondary}
-          label={{ position: "top" }}
           background={{ fill: UIProps.colors.charts.background }}
         />
+        <Tooltip content={<CustomTooltip />} />
       </BarChart>
     </Box>
   );
