e1802cf9f4fa4d1a2d314f1db330541a4473ecce
VVojtekMecki <wojtek.medenecki@gmail.com>
2024-09-26 21:54:24 +0200

    auth0 provider

--- src/main/webclient/src/client/app-wrapper.tsx
+++ src/main/webclient/src/client/app-wrapper.tsx
@@ -3,11 +3,9 @@ import { BrowserRouter } from "react-router-dom";
 import { MantineProvider, createTheme } from "@mantine/core";
 import { ChakraProvider, extendTheme, theme } from "@chakra-ui/react";
 import { UIProps } from "../config/config";
-import { Auth0Provider } from '@auth0/auth0-react';
-import config from '../config/config.json'
+import Auth0ProviderWithNavigation from "../auth/auth0-provider-with-navigation";
 
-
-export const AppWrapper = ({children}: {children: ReactNode}) => {
+export const AppWrapper = ({ children }: { children: ReactNode }) => {
   const ui = UIProps;
   const mantineTheme = createTheme({
     /** Put your mantine theme override here */
@@ -17,38 +15,31 @@ export const AppWrapper = ({children}: {children: ReactNode}) => {
     colors: {
       primary: {
         500: ui.colors.primary,
-        600: theme.colors.green[900]
+        600: theme.colors.green[900],
       },
       secondary: {
         500: ui.colors.secondary,
-        600: theme.colors.green[300]
+        600: theme.colors.green[300],
       },
       background: {
-        500: ui.colors.background
+        500: ui.colors.background,
       },
       accent: {
-        500: ui.colors.accent
+        500: ui.colors.accent,
       },
       deviceActiveTime: {
-        500: ui.colors.table.active
-      }
+        500: ui.colors.table.active,
+      },
     },
   });
 
   return (
-    <Auth0Provider
-    domain={config.auth0.domain}
-    clientId={config.auth0.clientID}
-    authorizationParams={{
-      redirect_uri: `${config.appVersions.LOCAL.APP_URL}`
-    }}
-  >
     <BrowserRouter>
-      <MantineProvider theme={mantineTheme}>
-        <ChakraProvider theme={chakraTheme}>{children}</ChakraProvider>
-      </MantineProvider>
+      <Auth0ProviderWithNavigation>
+        <MantineProvider theme={mantineTheme}>
+          <ChakraProvider theme={chakraTheme}>{children}</ChakraProvider>
+        </MantineProvider>
+      </Auth0ProviderWithNavigation>
     </BrowserRouter>
-  </Auth0Provider>
-    
   );
 };
