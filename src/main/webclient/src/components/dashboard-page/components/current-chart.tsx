22f88e225a62040c17125e79553b2be5015b0478
VVojtekMecki <wojtek.medenecki@gmail.com>
2024-10-09 19:18:41 +0200

    fix for current chart

--- src/main/webclient/src/components/dashboard-page/components/current-chart.tsx
+++ src/main/webclient/src/components/dashboard-page/components/current-chart.tsx
@@ -1,15 +1,18 @@
 import { HStack } from "@chakra-ui/react";
 import { StatusPieChart } from "./status-pie-chart";
 import { IChartTemplateModel } from "../../../types/chartTemplate";
+import { createDeviceModel } from "../../../types/deviceModel";
 
 
 export const CurrentChart = ({ model, devicesHistoryValues }: IChartTemplateModel) => {
+	const deviceModel = createDeviceModel(model);
+
 	if (devicesHistoryValues.length) {
 		return (
 			<HStack>
-			  <StatusPieChart devices={model.getSensorsArray()} heading="Sensors" />
-			  <StatusPieChart devices={model.getGatewaysArray()} heading="Gateways" />
-			  <StatusPieChart devices={model.getBridgesArray()} heading="Bridges" />
+			  <StatusPieChart devices={deviceModel.getSensorsArray()} heading="Sensors" />
+			  <StatusPieChart devices={deviceModel.getGatewaysArray()} heading="Gateways" />
+			  <StatusPieChart devices={deviceModel.getBridgesArray()} heading="Bridges" />
 			</HStack>
 		  );
 	} else {
