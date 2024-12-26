fa5b601d4b8e6c3b8b2d52cf1332e83398778b0a
VVojtekMecki <wojtek.medenecki@gmail.com>
2024-12-26 17:32:39 +0100

    Squashed commit of the following:
    commit a4728e2b7958dab9b8f435bde68566c70d812c20
    Author: sasankasa <michalikoliwiamagdalena@gmail.com>
    Date:   Thu Dec 19 20:39:13 2024 +0100
    
        usunięcie modalu
    
    commit 867a0390b40aae94d75886f880011aefe15035cd
    Author: sasankasa <michalikoliwiamagdalena@gmail.com>
    Date:   Thu Dec 19 20:12:20 2024 +0100
    
        dodany przycisk do utworzenia formatki emaila
    
    commit 2734e0a313b96ce65cde01b51e1193876a94e34e
    Author: sasankasa <michalikoliwiamagdalena@gmail.com>
    Date:   Thu Dec 19 20:12:02 2024 +0100
    
        poprawiony wygląd przycisku do logowania oraz landing page

--- src/main/webclient/src/components/landing-page/landing-page.tsx
+++ src/main/webclient/src/components/landing-page/landing-page.tsx
@@ -1,21 +1,15 @@
-import { Box, Center, Grid, GridItem, Heading } from "@chakra-ui/react";
+import { Center, Grid, GridItem, Heading } from "@chakra-ui/react";
 import { UIProps } from "../../config/config";
 
 export const LandingPage = () => {
   return (
-    <Box
-      paddingLeft="47px"
-      paddingRight="47px"
-      bg={UIProps.colors.background}
-      boxShadow="inner"
-    >
-      <Center>
-        <Grid templateRows="repeat(2, 1fr)" gap={20}>
-          <GridItem>
-            <Heading>Landing page</Heading>
-          </GridItem>
-        </Grid>
-      </Center>
-    </Box>
+    <Center bg={UIProps.colors.background} paddingTop="100px">
+      <Grid templateRows="repeat(2, 1fr)" gap={20}>
+        <GridItem>
+          <Heading>Welcome to our page. </Heading>
+          <Heading size="lg">Hope you find everything you need.</Heading>
+        </GridItem>
+      </Grid>
+    </Center>
   );
 };
