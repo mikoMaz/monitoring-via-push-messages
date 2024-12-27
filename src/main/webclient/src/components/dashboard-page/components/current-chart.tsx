e5b1701005324a2a007b0e69dc6fdfc7d6f741fa
sasankasa <michalikoliwiamagdalena@gmail.com>
2024-12-27 17:09:06 +0100

    wzięto pod uwagę zaznaczone checkboxy w rysowaniu current wykresu

--- src/main/webclient/src/components/dashboard-page/components/current-chart.tsx
+++ src/main/webclient/src/components/dashboard-page/components/current-chart.tsx
@@ -1,21 +1,39 @@
 import { HStack } from "@chakra-ui/react";
 import { StatusPieChart } from "./status-pie-chart";
-import { IChartTemplateModel, IChartTemplateModelDrawing } from "../../../types/chartTemplate";
+import { IChartTemplateModelDrawing } from "../../../types/chartTemplate";
 import { createDeviceModel } from "../../../types/deviceModel";
 
+export const CurrentChart = ({
+  model,
+  devicesHistoryValues,
+  deviceTypes,
+}: IChartTemplateModelDrawing & { deviceTypes: number[] }) => {
+  const deviceModel = createDeviceModel(model);
 
-export const CurrentChart = ({ model, devicesHistoryValues }: IChartTemplateModelDrawing) => {
-	const deviceModel = createDeviceModel(model);
-
-	if (devicesHistoryValues.length) {
-		return (
-			<HStack>
-			  <StatusPieChart devices={deviceModel.getSensorsArray()} heading="Sensors" />
-			  <StatusPieChart devices={deviceModel.getGatewaysArray()} heading="Gateways" />
-			  <StatusPieChart devices={deviceModel.getBridgesArray()} heading="Bridges" />
-			</HStack>
-		  );
-	} else {
-		return <>No data</>;
-	}
+  if (devicesHistoryValues.length) {
+    return (
+      <HStack>
+        {deviceTypes.includes(0) && (
+          <StatusPieChart
+            devices={deviceModel.getSensorsArray()}
+            heading="Sensors"
+          />
+        )}
+        {deviceTypes.includes(1) && (
+          <StatusPieChart
+            devices={deviceModel.getGatewaysArray()}
+            heading="Gateways"
+          />
+        )}
+        {deviceTypes.includes(2) && (
+          <StatusPieChart
+            devices={deviceModel.getBridgesArray()}
+            heading="Bridges"
+          />
+        )}
+      </HStack>
+    );
+  } else {
+    return <>No data</>;
+  }
 };
