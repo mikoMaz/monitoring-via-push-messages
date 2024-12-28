c6d2d0695a7a067bc9b59442be1ce87588dd5bdc
VVojtekMecki <wojtek.medenecki@gmail.com>
2024-12-28 12:59:13 +0100

    removed unused package mantine

--- src/main/webclient/src/client/app-wrapper.tsx
+++ src/main/webclient/src/client/app-wrapper.tsx
@@ -1,15 +1,11 @@
 import { ReactNode } from "react";
 import { BrowserRouter } from "react-router-dom";
-import { MantineProvider, createTheme } from "@mantine/core";
 import { ChakraProvider, extendTheme, theme } from "@chakra-ui/react";
 import { UIProps } from "../config/config";
 import Auth0ProviderWithNavigation from "../auth/auth0-provider-with-navigation";
 
 export const AppWrapper = ({ children }: { children: ReactNode }) => {
   const ui = UIProps;
-  const mantineTheme = createTheme({
-    /** Put your mantine theme override here */
-  });
 
   const chakraTheme = extendTheme({
     colors: {
@@ -36,9 +32,7 @@ export const AppWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <BrowserRouter>
       <Auth0ProviderWithNavigation>
-        <MantineProvider theme={mantineTheme}>
-          <ChakraProvider theme={chakraTheme}>{children}</ChakraProvider>
-        </MantineProvider>
+        <ChakraProvider theme={chakraTheme}>{children}</ChakraProvider>
       </Auth0ProviderWithNavigation>
     </BrowserRouter>
   );
