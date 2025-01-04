5373119ef099092848e242cf46a5f45eed768d95
VVojtekMecki <wojtek.medenecki@gmail.com>
2025-01-04 22:16:13 +0100

    switched object to interface

--- src/main/java/com/example/monitoring/core/api/WebDevicesModelController.java
+++ src/main/java/com/example/monitoring/core/api/WebDevicesModelController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.monitoring.core.api.abstraction.IDevicesModelService;
 import com.google.gson.JsonObject;
 
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WebDevicesModelController {
 
-    private final DevicesModelService devicesModelService;
+    private final IDevicesModelService devicesModelService;
 
     @GetMapping("/jsonTree")
     public ResponseEntity<String> jsonTree(@RequestParam String id) {
