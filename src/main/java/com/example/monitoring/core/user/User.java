c318a4be978db37e08b2d5bc01ce4de20eeff6c1
mikoMaz <mikmaz1@st.amu.edu.pl>
2025-01-12 23:29:51 +0100

    add sequence

--- src/main/java/com/example/monitoring/core/user/User.java
+++ src/main/java/com/example/monitoring/core/user/User.java
@@ -22,7 +22,8 @@ import java.util.List;
 public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_seq")
+    @SequenceGenerator(name = "users_id_seq", sequenceName = "users_id_seq", allocationSize = 1)
     private Long id;
 
     private String email;
