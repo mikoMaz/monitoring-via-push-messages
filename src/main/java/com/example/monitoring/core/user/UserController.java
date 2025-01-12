1af0b706577de528a32c179f710de5e72a92f8e5
mikoMaz <mikmaz1@st.amu.edu.pl>
2025-01-12 17:09:36 +0100

    MON-128 secure /create endpoint

--- src/main/java/com/example/monitoring/core/user/UserController.java
+++ src/main/java/com/example/monitoring/core/user/UserController.java
@@ -1,6 +1,7 @@
 package com.example.monitoring.core.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     final private UserService userService;
 
+    @PreAuthorize("(#user != null && #user.companyId != null && @userServiceImpl.hasRightToTheCompany(#user.companyId))")
     @PostMapping("/create")
     public void createUser(@RequestBody UserDtoBasic user) {
         userService.createUser(user);
